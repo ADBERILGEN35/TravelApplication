2068a5afd902a5c77266ff25d87c26e2f5dda557
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2022-10-11 15:33:36 +0300

    TravelApplication - SearchResult Fragment added.

---
+++ app/src/main/java/com/example/apptravel/view/SearchResultFragment.kt
@@ -0,0 +1,66 @@
+package com.example.apptravel.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.apptravel.R
+import com.example.apptravel.adapter.search.SearchHorizontalAdapter
+import com.example.apptravel.databinding.FragmentSearchResultBinding
+import com.example.apptravel.viewmodel.SearchFragmentViewModel
+import com.example.travelguideapplication.view.SearchResultFragmentArgs
+
+class SearchResultFragment : Fragment() {
+    private lateinit var binding: FragmentSearchResultBinding
+    private val searchViewModel: SearchFragmentViewModel by viewModels()
+    private var adapterHori: SearchHorizontalAdapter = SearchHorizontalAdapter(arrayListOf())
+    private val args: SearchResultFragmentArgs by navArgs()
+    private lateinit var searchText: String
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_search_result, container, false)
+        binding.buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.resultToSearch)
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        context?.let {
+
+        }
+        getSearch()
+    }
+
+    private fun getSearch() {
+        searchText = args.searchText
+        searchViewModel.searchData(searchText).observe(viewLifecycleOwner) { searchList ->
+            adapterHori.setTravelArrayList(searchList)
+            val gridLayoutManager =
+                GridLayoutManager(context, 1, GridLayoutManager.VERTICAL, false)
+            binding.recyclerViewResultHorizantal.layoutManager = gridLayoutManager
+            binding.recyclerViewResultHorizantal.adapter = adapterHori
+
+        }
+
+    }
+
+}
