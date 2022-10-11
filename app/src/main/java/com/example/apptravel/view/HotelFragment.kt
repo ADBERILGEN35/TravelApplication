3a5e7bb658663725d2d66ab5b865181c191553e1
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2022-10-11 14:21:26 +0300

    TravelApplication - Hotel Fragment added.

---
+++ app/src/main/java/com/example/apptravel/view/HotelFragment.kt
@@ -0,0 +1,55 @@
+package com.example.apptravel.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.apptravel.R
+
+class HotelFragment : Fragment() {
+
+    private lateinit var binding: FragmentHotelBinding
+    private val viewModel: HotelViewModel by viewModels()
+    private var adapter: TravelApiAdapter = TravelApiAdapter()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_hotel, container, false
+        )
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        context?.let {
+
+        }
+        getFlightData()
+    }
+
+
+    private fun getFlightData() {
+        viewModel.getTravelData().observe(viewLifecycleOwner) { travelValue ->
+            adapter.setTravelArrayList(travelValue)
+            val gridLayoutManager =
+                GridLayoutManager(context, 1, GridLayoutManager.HORIZONTAL, false)
+            binding.recyclerView.layoutManager = gridLayoutManager
+            binding.recyclerView.adapter = adapter
+        }
+    }
+
+
+}
